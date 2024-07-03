b9bd0dfaa87b79021d0aa574648b16796b6a37bb
Gustavo Ferreira <guuxts@gmail.com>
2024-07-03 20:29:19 -0300

    Initial Commit

---
+++ ChefDelivery-ProjectStudy/ChefDelivery_ProjectStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDelivery_ProjectStudyApp.swift
+//  ChefDelivery-ProjectStudy
+//
+//  Created by Gustavo Ferreira dos Santos on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDelivery_ProjectStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
