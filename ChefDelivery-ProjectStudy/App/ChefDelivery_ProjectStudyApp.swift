e228136ffbf5f6a8ae0d3e454bcbc571fefe62a5
Gustavo Ferreira <102172136+gustapng@users.noreply.github.com>
2025-03-24 21:11:36 -0300

    updating a project

--- ChefDelivery-ProjectStudy/App/ChefDelivery_ProjectStudyApp.swift
+++ ChefDelivery-ProjectStudy/App/ChefDelivery_ProjectStudyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ChefDelivery_ProjectStudyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ ChefDelivery-ProjectStudy/HomeView/HomeView.swift
@@ -0,0 +1,118 @@
+//
+//  HomeView.swift
+//  ChefDelivery-ProjectStudy
+//
+//  Created by Gustavo Ferreira dos Santos on 24/03/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    @State private var isAnimating = false
+    @State private var imageOffset: CGSize = .zero
+    let buttonHeight: CGFloat = 80
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Circle()
+                    .foregroundColor(Color("ColorRed"))
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(x: isAnimating ? 50 : -50, y: isAnimating ? 100 : -100)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 0)
+
+                Circle()
+                    .foregroundColor(Color("ColorRedDark"))
+                    .frame(width: isAnimating ? 200 : 0)
+                    .position(x: isAnimating ? geometry.size.width - 50 : geometry.size.width + 50,
+                              y:  isAnimating ? geometry.size.height - 50 : geometry.size.height + 50)
+                    .blur(radius: 60)
+                    .opacity(isAnimating ? 0.5 : 0)
+
+                VStack {
+                    Text("Chef Delivery")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color("ColorRed"))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+
+                    Text("Peça a sua comida no conforto da sua casa.")
+                        .font(.title2)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black.opacity(0.7))
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(y: isAnimating ? 0 : -40)
+
+                    Image("image")
+                        .resizable()
+                        .scaledToFit()
+                        .shadow(radius: 60)
+                        .padding(isAnimating ? 32 : 92)
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(x: imageOffset.width, y: imageOffset.height)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    withAnimation(.easeInOut(duration: 0.5)) {
+                                        imageOffset = gesture.translation
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    withAnimation(.easeInOut(duration: 0.5)) {
+                                        imageOffset = .zero
+                                    }
+                                })
+                        )
+                    
+                    ZStack {
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                        
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                            .padding(8)
+                        
+                        Text("Descubra mais")
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(Color("ColorRedDark"))
+                            .offset(x: 20)
+                        
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .fill(Color("ColorRed"))
+                                
+                                Circle()
+                                    .fill(Color("ColorRedDark"))
+                                    .padding(8)
+                                
+                                Image(systemName: "chevron.right.2")
+                                    .font(.system(size: 24))
+                                    .bold()
+                                    .foregroundColor(.white)
+                            }
+                            
+                            Spacer()
+                        }
+                    }
+                    .frame(width: geometry.size.width - 60, height: buttonHeight)
+                    .padding(8)
+                }
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 1.5)) {
+                        isAnimating = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
