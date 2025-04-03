cacbf85809b857d64ff3cc74804a7adcba49fe43
Gustavo Ferreira <102172136+gustapng@users.noreply.github.com>
2025-04-02 22:41:33 -0300

    updating a project

--- ChefDelivery-ProjectStudy/Networking/HomeService.swift
+++ ChefDelivery-ProjectStudy/Networking/HomeService.swift
@@ -26,4 +26,21 @@ struct HomeService {
 
         return .success(storesObjects)
     }
+
+    func confirmOrder(product: ProductType) async throws -> Result<[String: Any]?, RequestError> {
+        guard let url = URL(string: "https://private-1ebe6-chefdelivery27.apiary-mock.com/home") else {
+            return .failure(.invalidURL)
+        }
+
+        let encodedObject = try JSONEncoder().encode(product)
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = encodedObject
+
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let message = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+
+        return .success(message)
+    }
 }
